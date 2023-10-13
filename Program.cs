fe16b6e1bf645a047e36e4cf50416beb5b872cfd
MosutLarisa <larisa.mosut@emanuel.ro>
2023-10-13 20:41:16 +0300

    Impartire2numere

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Impartire2numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            if (a % b == 0)
+            {
+                Console.WriteLine(a / b);
+            }
+            else
+            {
+                Console.WriteLine("can not be calculated");
+            }
+        }
+        }
+    }
